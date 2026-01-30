f5b7d63f0029b799934ac2f9e01353974de25113
ThinkMuk <b2000kang@gmail.com>
2026-01-30 20:01:41 +0900

    fix(#1): LoadingScreen 컴포넌트 export 경로 수정

--- app/_components/main/screens/index.ts
+++ app/_components/main/screens/index.ts
@@ -1,4 +1,4 @@
 export { default as BeforeAnalysisScreen } from "./BeforeAnalysis";
-export { default as LoadingScreen } from "./Loading";
+export { default as LoadingScreen } from "./LoadingScreen";
 export { default as CompleteScreen } from "./Complete";
 
